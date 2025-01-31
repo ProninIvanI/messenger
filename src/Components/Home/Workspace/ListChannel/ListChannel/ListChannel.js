75d66baa08f4fd9b6dc5a15572ef70cb8065bf55
Pronin Denis <denis.pronin@sibers.com>
2025-01-31 22:15:15 +0700

    started creating adding channels to user

--- src/Components/Home/Workspace/ListChannel/ListChannel/ListChannel.js
+++ src/Components/Home/Workspace/ListChannel/ListChannel/ListChannel.js
@@ -3,29 +3,48 @@ import styles from "./ListChannel.module.css";
 import { ButtonChannel } from "../ButtonChannel/ButtonChannel";
 import { Search } from "../../Search/Search/Search";
 import { useState } from "react";
+import imageAddChannel from "../../../../../Images/AddChannel.svg";
 
 export function ListChannel() {
   const channels = useSelector(
     (state) => state.informationOfChannels.channelsUser
   );
+  const [addChannel, setAddChannel] = useState(false);
   const [sortedChannels, setSortedChannels] = useState(
     useSelector((state) => state.informationOfChannels.sortedChannelsUser)
   );
+  const handleClick = () => {
+    if (addChannel === false) {
+      setAddChannel(true); 
+    } else if (addChannel === true) {
+      setAddChannel(false); 
+    }
+  };
   const currentSearch = useSelector((state) => state.search.searchChannel);
   return (
     <div className={styles.container}>
-      <Search
-        array={channels}
-        currentSearch={currentSearch}
-        sortedArray={sortedChannels}
-        setSortedArray={setSortedChannels}
-        placeholder={"введите название"}
-        styleContainer={styles.containerSearch}
-      />
-      {sortedChannels &&
-        sortedChannels.map((channel) => (
-          <ButtonChannel key={channel} name={channel} />
-        ))}
+      <div className={styles.containerUnder}>
+        <Search
+          array={channels}
+          currentSearch={currentSearch}
+          sortedArray={sortedChannels}
+          setSortedArray={setSortedChannels}
+          placeholder={"введите название"}
+          styleContainer={styles.containerSearch}
+        />
+        {sortedChannels && addChannel === false ? (
+          sortedChannels.map((channel) => (
+            <ButtonChannel key={channel} name={channel} />
+          ))
+        ) : (
+          <div>mem</div>
+        )}
+      </div>
+      <div className={styles.containerUnderButton}>
+        <button className={styles.button} onClick={handleClick}>
+          <img alt="" src={imageAddChannel} />
+        </button>
+      </div>
     </div>
   );
 }
